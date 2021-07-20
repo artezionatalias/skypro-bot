63c1d1ae8ee2964231dc540de3c9241db9e51308
Natalie <Natalia.Sosunova@artezio.com>
2021-07-20 22:34:22 +0300

    Create simple_bot.py

---
+++ simple_bot.py
@@ -0,0 +1,12 @@
+from telegram_bot import TelegramBot
+
+
+bot = TelegramBot('YOUR TOKEN HERE')
+
+def hello():
+    bot.send_reply(f'Привет {bot.user_name}!')
+
+bot.add_command_handler('start', hello)
+
+
+bot.start()
